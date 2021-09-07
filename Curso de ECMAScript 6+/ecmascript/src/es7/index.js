6f2ce5f7fabf7ffda35755dcca59c9c0a6bc6995
Victor Gallegos <316160456@ciencias.unam.mx>
2021-09-07 14:35:36 -0500

    ¿Qué se implementó en ES7?

---
+++ Curso de ECMAScript 6+/ecmascript/src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 4, 5, 6];
+
+if (numbers.includes(6)) {
+  console.log("Si se encuentra el valor 6");
+} else {
+  console.log("Nose encuentra");
+}
+
+let base = 4;
+exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
