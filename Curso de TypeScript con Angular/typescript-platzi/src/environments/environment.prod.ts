59d9981c16d64598da8d6b4be7b0e01f443511c4
Victor Gallegos <316160456@ciencias.unam.mx>
2021-09-17 17:38:35 -0500

    Preparacion correcta del entorno

---
+++ Curso de TypeScript con Angular/typescript-platzi/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'typescript-platzi-78afd',
+    appId: '1:384604065441:web:0b377194960c526ee2aae0',
+    databaseURL: 'https://typescript-platzi-78afd-default-rtdb.firebaseio.com',
+    storageBucket: 'typescript-platzi-78afd.appspot.com',
+    apiKey: 'AIzaSyBADc2b0TCnJaVPGhHtffr7lR7e8FTahLc',
+    authDomain: 'typescript-platzi-78afd.firebaseapp.com',
+    messagingSenderId: '384604065441',
+    measurementId: 'G-TEKSW7PK5Z',
+  },
+  production: true
+};
---
+++ Curso de TypeScript con Angular/typescript-platzi/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'typescript-platzi-78afd',
+    appId: '1:384604065441:web:0b377194960c526ee2aae0',
+    databaseURL: 'https://typescript-platzi-78afd-default-rtdb.firebaseio.com',
+    storageBucket: 'typescript-platzi-78afd.appspot.com',
+    apiKey: 'AIzaSyBADc2b0TCnJaVPGhHtffr7lR7e8FTahLc',
+    authDomain: 'typescript-platzi-78afd.firebaseapp.com',
+    messagingSenderId: '384604065441',
+    measurementId: 'G-TEKSW7PK5Z',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
