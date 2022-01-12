da36897fefb79d7cafe60bfe2708cbd98deb98de
Victor Gallegos <316160456@ciencias.unam.mx>
2022-01-11 23:12:57 -0600

    nuevo modulo

--- Curso de Django/instagram/instagram/urls.py
+++ Curso de Django/instagram/instagram/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
